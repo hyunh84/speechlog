ad3ead5a104f83c1585842677860566823b846d5
hyunh84 <hyunh84@hanmail.net>
2021-08-09 09:46:54 +0900

    treemap chart

--- asset/js/chartUi.js
+++ asset/js/chartUi.js
@@ -1,3 +1,4 @@
+/* LINE CHART UI */
 var lineChartFn = function(target, options) {
 	var _wrap = $('#'+target).parent();
 	var _legendBox;
@@ -70,4 +71,59 @@ var lineChartFn = function(target, options) {
 	return {
 		chartFn : chartSet
 	}
+}
+
+/* AMCHART TREEMAP */
+var treeMapFn = function(target, options) {
+	var _target = $(target);
+	var _options = options || {};
+	// Themes begin
+	if(_options.theme === 'dataviz') am4core.useTheme(am4themes_dataviz);
+	am4core.useTheme(am4themes_animated);
+	// Themes end
+
+	// create chart
+	var chart = am4core.create(target, am4charts.TreeMap);
+	chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
+
+	chart.data = _options.data;
+
+	chart.colors.step = 2;
+
+	// define data fields
+	chart.dataFields.value = "value";
+	chart.dataFields.name = "name";
+	chart.dataFields.children = "children";
+
+	chart.zoomable = false;
+	var bgColor = new am4core.InterfaceColorSet().getFor("background");
+
+	// level 0 series template
+	var level0SeriesTemplate = chart.seriesTemplates.create("0");
+	var level0ColumnTemplate = level0SeriesTemplate.columns.template;
+
+	level0ColumnTemplate.column.cornerRadius(10, 10, 10, 10);
+	level0ColumnTemplate.fillOpacity = 0;
+	level0ColumnTemplate.strokeWidth = 4;
+	level0ColumnTemplate.strokeOpacity = 0;
+
+	// level 1 series template
+	var level1SeriesTemplate = chart.seriesTemplates.create("1");
+	var level1ColumnTemplate = level1SeriesTemplate.columns.template;
+
+	level1SeriesTemplate.tooltip.animationDuration = 0;
+	level1SeriesTemplate.strokeOpacity = 1;
+
+	level1ColumnTemplate.column.cornerRadius(10, 10, 10, 10)
+	level1ColumnTemplate.fillOpacity = 1;
+	level1ColumnTemplate.strokeWidth = 4;
+	level1ColumnTemplate.stroke = bgColor;
+
+	var bullet1 = level1SeriesTemplate.bullets.push(new am4charts.LabelBullet());
+	bullet1.locationY = 0.5;
+	bullet1.locationX = 0.5;
+	bullet1.label.text = "{name}";
+	bullet1.label.fill = am4core.color("#ffffff");
+
+	chart.maxLevels = 2;
 }
